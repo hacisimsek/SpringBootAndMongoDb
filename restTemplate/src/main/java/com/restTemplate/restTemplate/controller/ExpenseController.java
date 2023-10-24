1f600cc1051531bbe5589a10eba25381ee2bfe84
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2023-10-24 21:23:03 +0300

    first commit

---
+++ restTemplate/src/main/java/com/restTemplate/restTemplate/controller/ExpenseController.java
@@ -0,0 +1,55 @@
+package com.restTemplate.restTemplate.controller;
+
+import com.restTemplate.restTemplate.model.Expense;
+import com.restTemplate.restTemplate.service.ExpenseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.status;
+
+@RestController
+@RequestMapping("/api/expense")
+public class ExpenseController {
+    private final ExpenseService expenseService;
+
+    public ExpenseController(ExpenseService expenseService){
+        this.expenseService = expenseService;
+    }
+    @PostMapping
+    public ResponseEntity addExpense(@RequestBody Expense expense){
+        expenseService.addExpense(expense);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity updateExpense(@RequestBody Expense expense){
+        expenseService.updateExpense(expense);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteExpense(@PathVariable String id){
+        expenseService.deleteExpense(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Expense>> getAllExpenses(){
+        return ResponseEntity.ok(expenseService.getAllExpenses());
+    }
+    @GetMapping("/{name}")
+    public ResponseEntity<Expense> getExpenseByName(@PathVariable String name){
+        return ResponseEntity.ok(expenseService.getExpenseByName(name));
+    }
+
+}
---
+++ restTemplate/src/main/java/com/restTemplate/restTemplate/model/ExpenseCategory.java
@@ -0,0 +1,5 @@
+package com.restTemplate.restTemplate.model;
+
+public enum ExpenseCategory {
+    ENTERTAINMENT, GROCERIES, RESTAURANT, UTILITIES, MISC
+}
